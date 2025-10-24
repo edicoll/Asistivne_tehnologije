6d862aae3ac46e1c8e87d84612d41d968105b4a1
Edi <you@example.com>
2025-10-24 14:27:14 +0200

    Setting up projet

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+    const buttons = [
+        "Autizam",
+        "Slabovisnost",
+        "Slušni problemi",
+        "Settings",
+        "Notifications",
+        "Logout",
+    ];
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <div className="app-container">
+            <h1 className="app-title">Naziv aplikacije</h1>
+            <h2 className="description">Opis i odabir poteškoća</h2>
+            <div className="button-grid">
+                {buttons.map((label) => (
+                    <button key={label} className="app-button">
+                        {label}
+                    </button>
+                ))}
+            </div>
+        </div>
+    );
 }
 
 export default App
