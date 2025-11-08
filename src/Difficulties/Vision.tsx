881fe02318b13c871e3ebbb3d213248afb5f447b
Edi <you@example.com>
2025-11-08 19:04:59 +0100

    Dodana Dyslexia komponenta i kviz funkcionalnost

--- src/Difficulties/Vision.tsx
+++ src/Difficulties/Vision.tsx
@@ -6,6 +6,8 @@ function Vision() {
         <div className="page-container">
             <h2>Stranica: Vid</h2>
             <p>Ovdje možeš dodati sadržaj vezan uz vid.</p>
+            <h3>Pogledajmo s kojim se poteškoćama susreću osobe s slabovidnošću</h3>
+            <h3>Započinom kviz</h3>
             <button className="back-button" onClick={() => navigate("/")}>
                 ⬅ Povratak
             </button>
